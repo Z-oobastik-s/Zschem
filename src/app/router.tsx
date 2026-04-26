2ac569357ec15b8f5df63318608a4dfdacf76734
Zoobastiks <zoobastiks@users.noreply.github.com>
2026-04-26 03:52:17 +0300

    Update: 26.04.2026  3:52:17,68

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { ROUTES } from "@config/routes";
 import { lazy } from "react";
 
 const HomePage = lazy(() => import("@pages/HomePage"));
 const BuildDetailsPage = lazy(() => import("@pages/BuildDetailsPage"));
 
-export const appRouter = createBrowserRouter(
+export const appRouter = createHashRouter(
   [
     {
       path: ROUTES.home,
